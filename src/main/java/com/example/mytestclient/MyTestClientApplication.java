cf1c39b117bf6070850e7a5dd8628ec3d613507c
ouyangshixiong <ouyangshixiong@gmail.com>
2019-08-21 18:14:06 +0800

    SPI加载配置挪到了apollo

--- src/main/java/com/example/mytestclient/MyTestClientApplication.java
+++ src/main/java/com/example/mytestclient/MyTestClientApplication.java
@@ -1,9 +1,12 @@
 package com.example.mytestclient;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class MyTestClientApplication {
 
     public static void main(String[] args) {
